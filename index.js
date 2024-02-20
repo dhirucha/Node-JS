39ce2ac6f2ae12c195f1ab6f12e219ea1875c0b9
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2024-02-20 22:58:59 +0530

    Read file nodeJS

--- index.js
+++ index.js
@@ -24,4 +24,24 @@ console.log('dirName >',dirName)
 
 //**********Lecture 2 (built in function module(Node-js)************* **/
 
+//Asynchronous way to read the file
+ const fs = require('fs')
 
+// console.log('READ START')
+// fs.readFile('input.txt', function(err,data){
+//     if(err){
+//         console.log('Error',err)
+//         return err;
+//     }
+
+//     console.log('data',data.toString())
+//     console.log('READ END')
+//     return data;
+// });
+
+
+//synchronous way to read the file
+const data = fs.readFileSync('input.txt')
+console.log('Data: ',data.toString())
+console.log('READ END')
+console.log('Other stuff')
