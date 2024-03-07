23df82f2bd63b841d064aaca4f657fca3ba61112
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2024-03-07 23:01:32 +0530

    some changes

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,8 @@ const PORT = 3020
 const HOSTNAME = 'localhost'
 
 const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('content-type','text/plain')
     res.end('Node server created by Dheeraj chaubey')
 })
 
